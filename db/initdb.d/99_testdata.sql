d780d6069b14a99aa0d8f7971976f3302beaa42f
Christian Dalvit <chris.dalvit@gmail.com>
2025-10-12 20:39:34 +0200

    insert more test data

--- db/initdb.d/99_testdata.sql
+++ db/initdb.d/99_testdata.sql
@@ -1,1 +1,5 @@
-INSERT INTO "user" (id, email, password) VALUES (1, 'admin@admin.com', 'admin');
+INSERT INTO "user" (email, password) VALUES ('admin@admin.com', 'admin');
+
+INSERT INTO "farm" (name) VALUES ( 'TestBetrieb' );
+
+INSERT INTO "orchard" (name, fram_id) VALUES ( 'TestAnlage', 1 );
